576264a8f2c7dadfa4aca19c775b9572b272a79c
menezesr08 <menezesr08@gmail.com>
2021-06-01 07:11:20 +0100

    updated files from codesandbox

---
+++ Number-of-good-pairs/Problem/index.js
@@ -0,0 +1,74 @@
+const inputContainer = document.getElementById("input-container");
+let numGoodPairsTag = document.getElementById("pairs");
+var startBtn = document.getElementById("startBtn");
+generateInputBlocks();
+startBtn.addEventListener("click", function () {
+  $("label").remove();
+  $(".block").remove();
+  startBtn.disabled = true;
+  generateInputBlocks();
+  solution();
+});
+
+function generateInputBlocks() {
+  let nums = [1, 2, 3, 1, 1, 3];
+  let standardLabelMargin = 51;
+  let label = createIndexLabel(0, 22);
+  let block = createBlock(0, nums[0]);
+  inputContainer.appendChild(label);
+  inputContainer.appendChild(block);
+
+  for (let i = 1; i < nums.length; i++) {
+    let label = createIndexLabel(i, standardLabelMargin);
+    let block = createBlock(i, nums[i]);
+    inputContainer.appendChild(label);
+    inputContainer.appendChild(block);
+  }
+}
+
+async function solution() {
+  let numOccurences = {};
+  let numGoodPairs = 0;
+  let inputBlocks = inputContainer.querySelectorAll(".block");
+
+  for (let i = 0; i < inputBlocks.length; i += 1) {
+    inputBlocks[i].style.backgroundColor = "#58B7FF";
+    const blockValue = inputBlocks[i].firstElementChild.innerHTML;
+    const value = parseInt(blockValue, 10);
+    if (value in numOccurences) {
+      inputBlocks[i].style.backgroundColor = "#FFFF00";
+      numGoodPairs += numOccurences[value];
+      numOccurences[value] += 1;
+    } else {
+      numOccurences[value] = 1;
+    }
+    numGoodPairsTag.innerHTML = numGoodPairs;
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 2000)
+    );
+
+    inputBlocks[i].style.backgroundColor = "#ff5858";
+  }
+  startBtn.disabled = false;
+}
+
+function createBlock(i, label) {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.transform = `translateX(${i * 60}px)`;
+
+  const blockLabel = document.createElement("label");
+  blockLabel.innerHTML = label;
+  block.appendChild(blockLabel);
+
+  return block;
+}
+
+function createIndexLabel(i, margin) {
+  const label = document.createElement("label");
+  label.innerHTML = i;
+  label.style.marginLeft = `${margin}px`;
+  return label;
+}
