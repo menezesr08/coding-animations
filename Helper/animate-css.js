632596e5652407c92bc94d0e2ee6deffbe5d4806
menezesr08 <menezesr08@gmail.com>
2021-06-05 15:23:36 +0100

    added helper file for animate css

---
+++ Helper/animate-css.js
@@ -0,0 +1,18 @@
+export const animateCSS = (node, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+
+    node.classList.add(`${prefix}animated`, animationName);
+    node.style.setProperty("--animate-duration", "3s");
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      node.style.removeProperty("--animate-duration");
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
+
